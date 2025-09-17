28f462bda747acac8622b6578026d93a59aeee5b
Shefii-02 <shefii.km@gmail.com>
2025-09-17 19:03:48 +0530

    StudentPersonalInfo

--- app/Models/StudentPersonalInfo.php
+++ app/Models/StudentPersonalInfo.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentPersonalInfo extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $table = "student_personal_info";
+  protected $table = "student_personal_info";
+  protected $fillable = ['student_id', 'parent_email', 'parent_mobile', 'parent_name', 'parent_relation', 'current_eduction', 'study_mode'];
 }

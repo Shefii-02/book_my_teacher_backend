215bd18de7ede68b0c17a62658a739609a98a62e
Shefii-02 <shefii.km@gmail.com>
2025-11-11 20:33:06 +0530

    test

--- app/Http/Resources/TeacherPersonalResource.php
+++ app/Http/Resources/TeacherPersonalResource.php
@@ -9,14 +9,14 @@ class TeacherPersonalResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'full_name' => $this->full_name,
-            'email' => $this->email,
-            'address' => $this->address,
-            'city' => $this->city,
-            'postal_code' => $this->postal_code,
-            'district' => $this->district,
-            'state' => $this->state,
-            'country' => $this->country,
+            'full_name' => $this->full_name ?? '',
+            'email' => $this->email ?? '',
+            'address' => $this->address ?? '',
+            'city' => $this->city ?? '',
+            'postal_code' => $this->postal_code ?? '',
+            'district' => $this->district ?? '',
+            'state' => $this->state ?? '',
+            'country' => $this->country ?? '',
         ];
     }
 }

ad4fe3de954abd7439722f757c7e1f3bb3ba031f
Shefii-02 <shefii.km@gmail.com>
2025-11-11 11:06:18 +0530

    userDataRetrieve

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -160,6 +160,7 @@ class UserController extends Controller
 
   public function  userDataRetrieve(Request $request)
   {
+    Log::alert('userDataRetrieve');
     Log::info($request->user());
     try {
       $user = $request->user();

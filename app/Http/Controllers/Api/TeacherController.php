ec8e04c7257af8bd7e28751ea46b5a1c21f53d66
Shefii-02 <shefii.km@gmail.com>
2025-11-18 22:10:44 +0530

    etst

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -909,10 +909,10 @@ class TeacherController extends Controller
       'message' => 'Achievement level fetched',
       'data' => [
         'current_level' => 2,
-        'current_points' => 5200,
+        'current_points' => 2200,
         'next_level' => 3,
         'points_to_reach_next' => 6000,
-        'points_needed_for_next' => 800, // 6000 - 5200
+        'points_needed_for_next' => 3800, // 6000 - 5200
         'tasks' => [
           ['title' => 'Complete Daily Practice', 'completed' => true],
           ['title' => 'Attend Live Class', 'completed' => false],

12238d18bbd5eff4c0661590fff1a7f096cd360f
Shefii-02 <shefii.km@gmail.com>
2025-12-05 21:07:45 +0530

    teachersListing

--- app/Http/Resources/TeacherResource.php
+++ app/Http/Resources/TeacherResource.php
@@ -6,19 +6,22 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TeacherResource extends JsonResource
 {
-  public function toArray($request) {
+  public function toArray($request)
+  {
 
-        return [
-            'id' => (int) $this->id,
-            'name' => $this->name,
-            'qualification' => $this->qualifications ?? '',
-            'subjects' => is_array($this->whenLoaded('subjects')->pluck('name') ? implode(',',$this->subjects) : ''),
-            'courses' => [],
-            'ranking' => intval($this->ranking ?? '1'),
-            'rating' => floatval($this->rating ?? '4.5'),
-            'imageUrl' => asset($this->thumbnail_url),
-            'description' => $this->description,
-            'reviews' => ReviewResource::collection($this->whenLoaded('reviews')),
-        ];
+    return [
+      'id' => (int) $this->id,
+      'name' => $this->name,
+      'qualification' => $this->qualifications ?? '',
+      'subjects'      => $this->whenLoaded('subjects', function () {
+        return $this->subjects->pluck('name')->implode(',');
+      }),
+      'courses' => [],
+      'ranking' => intval($this->ranking ?? '1'),
+      'rating' => floatval($this->rating ?? '4.5'),
+      'imageUrl' => asset($this->thumbnail_url),
+      'description' => $this->description,
+      'reviews' => ReviewResource::collection($this->whenLoaded('reviews')),
+    ];
   }
 }

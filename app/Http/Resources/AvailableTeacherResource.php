06188e4ccfe51bb35a84ef46605e9213aa18ec79
Shefii-02 <shefii.km@gmail.com>
2025-12-06 00:35:03 +0530

    AvailableTeacherResource

--- app/Http/Resources/AvailableTeacherResource.php
+++ app/Http/Resources/AvailableTeacherResource.php
@@ -6,16 +6,19 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class AvailableTeacherResource extends JsonResource
 {
-    public function toArray($request)
-    {
-        return [
-            'id' => (int) $this->id,
-            'name' => $this->name,
-            'qualification' => $this->qualifications ?? '',
-            'subjects' => is_array($this->subjects) ? implode(',',$this->subjects) : '',
-            'ranking' => intval($this->ranking ?? '1'),
-            'rating' => floatval($this->rating ?? '4.5'),
-            'imageUrl' => asset($this->thumbnail_url),
-        ];
-    }
+  public function toArray($request)
+  {
+    return [
+      'id' => (int) $this->id,
+      'name' => $this->name,
+      'qualification' => $this->qualifications ?? '',
+      // 'subjects' => is_array($this->subjects) ? implode(',',$this->subjects) : '',
+      'subjects' =>  $this->whenLoaded('selectedSubjects', function () {
+        return $this->selectedSubjects->pluck('name')->implode(',');
+      }),
+      'ranking' => intval($this->ranking ?? '1'),
+      'rating' => floatval($this->rating ?? '4.5'),
+      'imageUrl' => asset($this->thumbnail_url),
+    ];
+  }
 }

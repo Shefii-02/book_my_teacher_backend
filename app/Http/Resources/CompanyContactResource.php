c744942fc9a487047ca3c8aaf102bff49d18925f
Shefii-02 <shefii.km@gmail.com>
2025-12-05 23:31:08 +0530

    test

--- app/Http/Resources/CompanyContactResource.php
+++ app/Http/Resources/CompanyContactResource.php
@@ -9,16 +9,11 @@ class CompanyContactResource extends JsonResource
   public function toArray($request)
   {
     return [
-      // 'email' => $this->email,
-      // 'phone' => $this->phone,
-      // 'whatsapp' => $this->whatsapp,
-      // 'website' => $this->website,
-      // 'address' => $this->address
-      "email" => "support@bookmyteacher.com",
-      "phone" => "+91 98765 43210",
-      "whatsapp" => "917510114455",
-      "website" => "https://bookmyteacher.co.in",
-      "address" => "Trivandrum, Kerala, India"
+      'email' => $this->email,
+      'phone' => $this->phone,
+      'whatsapp' => $this->whatsapp,
+      'website' => $this->website,
+      'address' => $this->address
     ];
   }
 }

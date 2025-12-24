7200099a9eb3b69d6e23105c2e101dcdebbd4360
Shefii-02 <shefii.km@gmail.com>
2025-12-24 15:58:25 +0530

    fix

--- resources/views/company/workshops/form.blade.php
+++ resources/views/company/workshops/form.blade.php
@@ -87,7 +87,7 @@
                             @foreach ($users as $user)
                                 <option value="{{$user['id']}}"
                                     {{ old('host_id', $isEdit ? $workshop->host_id : '') == $user['id'] ? 'selected' : '' }}>
-                                    {{ $user['name'].'('.str_replace('_',$user['type']).')' }}</option>
+                                    {{ $user['name'].'('.str_replace('_',' ',$user['type']).')' }}</option>
                             @endforeach
                         </select>
                         @error('host_id')

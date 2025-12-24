fb6ead00b8f1a7d191fae4bf0de0a06973d1cc7b
Shefii-02 <shefii.km@gmail.com>
2025-12-25 01:39:29 +0530

    course_registrations

---
+++ database/migrations/2025_12_25_284741_add_new_column_course_registrations_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+
+
+    Schema::table('course_registrations', function (Blueprint $table) {
+      $table->unsignedBigInteger('company_id')->nullable()->after('id')->index();
+      $table->foreign('company_id')->references('id')->on('companies')->cascadeOnDelete();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void {}
+};

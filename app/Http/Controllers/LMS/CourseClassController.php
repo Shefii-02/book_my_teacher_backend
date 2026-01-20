0aef178dc5c9ea7f5e73d0b8ccbb600b622a2b45
Shefii-02 <shefii.km@gmail.com>
2026-01-20 14:51:45 +0530

    fix

--- app/Http/Controllers/LMS/CourseClassController.php
+++ app/Http/Controllers/LMS/CourseClassController.php
@@ -10,6 +10,7 @@ use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class CourseClassController extends Controller
 {

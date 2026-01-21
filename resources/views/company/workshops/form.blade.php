d65d69c8001742b352d27573516d11c2722e2208
Shefii-02 <shefii.km@gmail.com>
2026-01-21 14:49:14 +0530

    fix

--- resources/views/company/workshops/form.blade.php
+++ resources/views/company/workshops/form.blade.php
@@ -100,7 +100,7 @@
                             <option value="">-- Select Host --</option>
                             @foreach ($users ?? [] as $user)
                                 <option value="{{ $user['user_id'] }}"
-                                    {{ old('host_id', $isEdit ? $workshop->host_id : '') == $user['id'] ? 'selected' : '' }}>
+                                    {{ old('host_id', $isEdit ? $workshop->host_id : '') == $user['user_id'] ? 'selected' : '' }}>
                                     {{ $user['name'] . '(' . $user['type'] . ')' }}</option>
                             @endforeach
                         </select>

e13540c6d93a324c6797a43e4792131979346b5c
Shefii-02 <shefii.km@gmail.com>
2026-01-30 11:09:13 +0530

    fix

--- app/Http/Controllers/MobileApp/ReferralController.php
+++ app/Http/Controllers/MobileApp/ReferralController.php
@@ -16,50 +16,50 @@ class ReferralController extends Controller
   public function index(Request $request)
   {
     $search = $request->search;
-// $users = AppReferral::query()
-//     ->leftJoin('users', 'app_referrals.ref_user_id', '=', 'users.id')
+$users = AppReferral::query()
+    ->leftJoin('users', 'app_referrals.ref_user_id', '=', 'users.id')
 
-//     ->when($search, function ($q) use ($search) {
-//         $q->where(function ($inner) use ($search) {
-//             $inner->where('users.name', 'LIKE', "%{$search}%")
-//                 ->orWhere('users.email', 'LIKE', "%{$search}%")
-//                 ->orWhere('users.mobile', 'LIKE', "%{$search}%")
-//                 ->orWhere('users.referral_code', 'LIKE', "%{$search}%");
-//         });
-//     })
-
-//     ->groupBy('app_referrals.ref_user_id')
-
-//     ->select([
-//         'app_referrals.ref_user_id as id',
-
-//         DB::raw("COALESCE(users.name, 'Deleted User') as name"),
-//         'users.email',
-//         'users.mobile',
-//         'users.referral_code',
-
-//         DB::raw('COUNT(app_referrals.id) as total_visits'),
-//         DB::raw('SUM(app_referrals.applied = 1) as joined_users'),
-//     ])
-
-//     ->paginate(20);
-
-    $users = AppReferral::with('user')
     ->when($search, function ($q) use ($search) {
-        $q->whereHas('user', function ($u) use ($search) {
-            $u->where('name', 'LIKE', "%$search%")
-              ->orWhere('email', 'LIKE', "%$search%")
-              ->orWhere('mobile', 'LIKE', "%$search%")
-              ->orWhere('referral_code', 'LIKE', "%$search%");
+        $q->where(function ($inner) use ($search) {
+            $inner->where('users.name', 'LIKE', "%{$search}%")
+                ->orWhere('users.email', 'LIKE', "%{$search}%")
+                ->orWhere('users.mobile', 'LIKE', "%{$search}%")
+                ->orWhere('users.referral_code', 'LIKE', "%{$search}%");
         });
     })
-    ->select(
-        'ref_user_id',
-        DB::raw('COUNT(id) as total_visits'),
-        DB::raw('SUM(applied = 1) as joined_users')
-    )
-    ->groupBy('ref_user_id')
+
+    ->groupBy('app_referrals.ref_user_id')
+
+    ->select([
+        'app_referrals.ref_user_id as id',
+
+        DB::raw("COALESCE(users.name, 'Deleted User') as name"),
+        'users.email',
+        'users.mobile',
+        'users.referral_code',
+
+        DB::raw('COUNT(app_referrals.id) as total_visits'),
+        DB::raw('SUM(app_referrals.applied = 1) as joined_users'),
+    ])
+
     ->paginate(20);
+
+    // $users = AppReferral::with('user')
+    // ->when($search, function ($q) use ($search) {
+    //     $q->whereHas('user', function ($u) use ($search) {
+    //         $u->where('name', 'LIKE', "%$search%")
+    //           ->orWhere('email', 'LIKE', "%$search%")
+    //           ->orWhere('mobile', 'LIKE', "%$search%")
+    //           ->orWhere('referral_code', 'LIKE', "%$search%");
+    //     });
+    // })
+    // ->select(
+    //     'ref_user_id',
+    //     DB::raw('COUNT(id) as total_visits'),
+    //     DB::raw('SUM(applied = 1) as joined_users')
+    // )
+    // ->groupBy('ref_user_id')
+    // ->paginate(20);
 
 
     return view('company.mobile-app.referrals.index', compact('users'));

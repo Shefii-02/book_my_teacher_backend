fe0f93242bff15ad3bb54a9afc19ed438d2d925a
Shefii-02 <shefii.km@gmail.com>
2026-02-01 20:49:49 +0530

    fix

--- resources/views/company/academic/admissions/form.blade.php
+++ resources/views/company/academic/admissions/form.blade.php
@@ -199,7 +199,7 @@
                     </div>
 
                     <!-- Right: Fixed (col-3) w-4/12-->
-                    <div class=" h-screen  overflow-auto sticky top-0 bg-white border-l px-1.5">
+                    <div class="  overflow-auto sticky top-0 bg-white border-l px-1.5">
 
                         {{-- Price / tax / totals --}}
                         <div class="flex flex-row gap-3 grid-cols-4 items-start mb-4 w-full">

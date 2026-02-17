6d8f21e0ed7a1e283ae8ec8e1a30c1e956e20913
Shefii-02 <shefii.km@gmail.com>
2026-02-18 03:22:53 +0530

    fix

--- database/migrations/2026_02_07_071019_modify_subject_id_in_subject_reviews_table.php
+++ database/migrations/2026_02_07_071019_modify_subject_id_in_subject_reviews_table.php
@@ -9,12 +9,12 @@ return new class extends Migration {
   {
     Schema::table('subject_reviews', function (Blueprint $table) {
 
-      $table->dropUnique('subject_reviews_subject_id_foreign');
-      $table->string('subject_id')->nullable()->change();
-      $table->dropForeign('subject_reviews_subject_course_id_foreign');
+      // $table->dropUnique('subject_reviews_subject_id_foreign');
+      // $table->string('subject_id')->nullable()->change();
+      // $table->dropForeign('subject_reviews_subject_course_id_foreign');
 
-      $table->dropUnique('subject_reviews_subject_course_id_foreign');
-      $table->renameColumn('subject_course_id', 'course_id');
+      // $table->dropUnique('subject_reviews_subject_course_id_foreign');
+      // $table->renameColumn('subject_course_id', 'course_id');
 
     });
   }

12d296ce641a0f95348355bcae130ad7a032f5f0
Shefii-02 <shefii.km@gmail.com>
2026-02-20 09:47:32 +0530

    migration

---
+++ database/migrations/2026_02_07_071019_modify_course_teachers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  public function up()
+  {
+    Schema::table('teacher_courses', function (Blueprint $table) {
+
+
+    // Drop unique composite key
+    $table->dropUnique('teacher_courses_teacher_id_course_id_unique');
+
+    // Make teacher nullable
+    $table->unsignedBigInteger('teacher_id')->nullable()->change();
+    });
+  }
+
+
+
+
+  public function down()
+  {
+    Schema::table('teacher_courses', function (Blueprint $table) {});
+  }
+};

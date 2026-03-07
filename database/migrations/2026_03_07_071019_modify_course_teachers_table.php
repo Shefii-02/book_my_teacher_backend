d6c8890dc6a1175d066ca28fef95e14d30b76253
Shefii-02 <shefii.km@gmail.com>
2026-03-07 14:21:17 +0530

    fix

---
+++ database/migrations/2026_03_07_071019_modify_course_teachers_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  public function up()
+  {
+    Schema::table('teacher_classes', function (Blueprint $table) {
+
+
+      // Drop unique composite key
+      $table->dropForeign(['teacher_id']);
+    });
+  }
+
+
+
+
+  public function down()
+  {
+    Schema::table('teacher_classes', function (Blueprint $table) {});
+  }
+};

aed39e012c88e113b3d43e17ea1859266932ef78
Shefii-02 <shefii.km@gmail.com>
2026-03-10 11:17:12 +0530

    fix

--- app/Models/DemoClassRegistration.php
+++ app/Models/DemoClassRegistration.php
@@ -7,19 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class DemoClassRegistration extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = [
-        'class_id','user_id','name','email','phone','checked_in','attended_status',
-    ];
+  protected $fillable = [
+    'class_id',
+    'user_id',
+    'name',
+    'email',
+    'phone',
+    'checked_in',
+    'attended_status',
+  ];
 
-    public function demoClass()
-    {
-        return $this->belongsTo(DemoClass::class);
-    }
+  public function demoClass()
+  {
+    return $this->belongsTo(DemoClass::class, 'class_id', 'id');
+  }
 
-    public function user()
-    {
-        return $this->hasOne(User::class,'id','user_id');
-    }
+  public function user()
+  {
+    return $this->hasOne(User::class, 'id', 'user_id');
+  }
 }

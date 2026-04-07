7ed21b34ba714108d94c7d56fa08385acf54d345
Shefii-02 <shefii.km@gmail.com>
2026-04-07 23:44:44 +0530

    fix

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -772,7 +772,7 @@ class UserController extends Controller
       }
     }
 
-    Log::info($model->provider->name);
+
     // ✅ Fix: $model IS the course_class already when type is Course
     if ($type == 'Course') {
       $parent = $model->course_classes;
@@ -805,7 +805,7 @@ class UserController extends Controller
       "time"         => $start->format('h:i a'),
       "start_time"   => $start->format('d-m-Y h:i a'),
       "end_time"     => $end->format('d-m-Y h:i a'),
-      "platform"     => $model->provider?->name ?? $model->class_mode ?? '',
+      "platform"     => $model->provider?->slug ?? $model->class_mode ?? '',
       "subject"      => $model->description ?? '',
       "course"       => $course?->title ?? '',
       "teacher_name" => $model->host?->name ?? '',

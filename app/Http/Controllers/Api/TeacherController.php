f1497bdc90cfa47566d53000999a847463172984
Shefii-02 <shefii.km@gmail.com>
2026-04-09 13:08:44 +0530

    fix

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -1017,7 +1017,7 @@ class TeacherController extends Controller
       'actual_price' => "0",
       'price' => "0",
       'course_type' => "0",
-      'instructors' => [['id' => 1,'avatar'=>'','name'=>'aKM','specialization'=>'SM,LS,MS']],
+      'instructors' => [],
       'started_at' => "10-01-2026 12:00:00 AM",
       'ended_at' => "12-10-2026 11:59:00 PM",
       'counselling_section' => true,

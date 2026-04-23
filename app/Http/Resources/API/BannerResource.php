1cccaa945dd9576c70da9124fd7195f886a616cc
Shefii-02 <shefii.km@gmail.com>
2026-04-23 23:13:49 +0530

    fix

--- app/Http/Resources/API/BannerResource.php
+++ app/Http/Resources/API/BannerResource.php
@@ -6,108 +6,153 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BannerResource extends JsonResource
 {
-    public function toArray($request)
-    {
-        $userBooking = $this->whenLoaded('requestBanner');
+  public function toArray($request)
+  {
+    $userBooking = $this->whenLoaded('requestBanner');
 
-        return [
-            'id'             => (int)$this->id,
-            'title'          => $this->title,
-            'description'    => $this->description,
-            'section_id'     => $this->section_id,
+    return [
+      'id'             => (int)$this->id,
+      'title'          => $this->title,
+      'description'    => $this->description,
+      'section_id'     => $this->section_id,
 
-            'main_image'     => $this->main_id
-                                ? $this->main_image_url
-                                : null,
+      'main_image'     => $this->main_id
+        ? $this->main_image_url
+        : null,
 
-            'thumb'          => $this->thumb_id
-                                ? $this->thumbnail_url
-                                : null,
+      'thumb'          => $this->thumb_id
+        ? $this->thumbnail_url
+        : null,
 
-            'priority_order' => $this->priority,
-            'banner_type'    => $this->banner_type,
+      'priority_order' => $this->priority,
+      'banner_type'    => $this->banner_type,
 
-            'cta_label'      => $this->cta_label,
-            'cta_action'     => $this->cta_action,
+      'cta_label'      => $this->cta_label,
+      'cta_action'     => $this->cta_action,
 
-            'type'           => $this->type,
+      'type'           => $this->type,
 
-            // booking request
-            'is_booked'      => !empty($userBooking),
-            'last_booked_at' => $userBooking->created_at ?? null,
+      // booking request
+      'is_booked'      => !empty($userBooking),
+      'last_booked_at' => $userBooking->created_at ?? null,
 
-            // enrollment / registration flags
-            'is_enrolled'    => (bool) ($this->is_enrolled ?? false),
+      // enrollment / registration flags
+      // 'is_enrolled'    => (bool) ($this->is_enrolled ?? false),
+      'is_enrolled' => $this->isUserEnrolled(),
+      'is_registered' => $this->isUserRegistered(),
 
-            'is_registered'  => $this->resolveRegistrationStatus(),
+      // 'is_registered'  => $this->resolveRegistrationStatus(),
 
-            // optional button text for app
-            'action_status'  => $this->resolveActionStatus(),
+      // optional button text for app
+      'action_status'  => $this->resolveActionStatus(),
 
-            // section details
-            'type_details'   => $this->getTypeDetails(),
-        ];
+      // section details
+      'type_details'   => $this->getTypeDetails(),
+    ];
+  }
+
+
+  protected function resolveRegistrationStatus()
+  {
+    switch ($this->type) {
+
+      case 'course':
+        return (bool) ($this->is_course_registered ?? false);
+
+      case 'webinar':
+        return (bool) ($this->is_webinar_registered ?? false);
+
+      case 'workshop':
+        return (bool) ($this->is_workshop_registered ?? false);
+
+      default:
+        return false;
+    }
+  }
+
+
+  protected function resolveActionStatus()
+  {
+    if ($this->is_enrolled) {
+      return 'continue_learning';
     }
 
-
-    protected function resolveRegistrationStatus()
-    {
-        switch ($this->type) {
-
-            case 'course':
-                return (bool) ($this->is_course_registered ?? false);
-
-            case 'webinar':
-                return (bool) ($this->is_webinar_registered ?? false);
-
-            case 'workshop':
-                return (bool) ($this->is_workshop_registered ?? false);
-
-            default:
-                return false;
-        }
+    if ($this->resolveRegistrationStatus()) {
+      return 'registered';
     }
 
-
-    protected function resolveActionStatus()
-    {
-        if ($this->is_enrolled) {
-            return 'continue_learning';
-        }
-
-        if ($this->resolveRegistrationStatus()) {
-            return 'registered';
-        }
-
-        if ($this->is_booked) {
-            return 'booked';
-        }
-
-        return 'apply_now';
+    if ($this->is_booked) {
+      return 'booked';
     }
 
+    return 'apply_now';
+  }
 
-    protected function getTypeDetails()
-    {
-        switch ($this->type) {
 
-            case 'course':
-                return $this->course
-                    ? new CourseResource($this->course)
-                    : null;
+  protected function getTypeDetails()
+  {
+    switch ($this->type) {
 
-            case 'workshop':
-                return $this->workshop
-                    ? new WorkshopResource($this->workshop)
-                    : null;
+      case 'course':
+        return $this->course
+          ? new CourseResource($this->course)
+          : null;
 
-            case 'webinar':
-                return $this->webinar
-                    ? new WebinarResource($this->webinar)
-                    : null;
+      case 'workshop':
+        return $this->workshop
+          ? new WorkshopResource($this->workshop)
+          : null;
 
-            default:
-                return null;
-        }
+      case 'webinar':
+        return $this->webinar
+          ? new WebinarResource($this->webinar)
+          : null;
+
+      default:
+        return null;
     }
+  }
+
+
+  protected function isUserEnrolled()
+  {
+    switch ($this->type) {
+
+      case 'course':
+        return $this->courseEntolled
+          && $this->courseEntolled->count() > 0;
+
+      case 'webinar':
+        return $this->webinarRegistration
+          && $this->webinarRegistration->count() > 0;
+
+      case 'workshop':
+        return $this->workshopRegistration
+          && $this->workshopRegistration->count() > 0;
+
+      default:
+        return false;
+    }
+  }
+
+  protected function isUserRegistered()
+  {
+    switch ($this->type) {
+
+      case 'course':
+        return $this->courseRegistration
+          && $this->courseRegistration->count() > 0;
+
+      case 'webinar':
+        return $this->webinarRegistration
+          && $this->webinarRegistration->count() > 0;
+
+      case 'workshop':
+        return $this->workshopRegistration
+          && $this->workshopRegistration->count() > 0;
+
+      default:
+        return false;
+    }
+  }
 }

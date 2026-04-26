6b9a9b0cf41ccf1a42b0984bd6ebf03e06e2cc20
Shefii-02 <shefii.km@gmail.com>
2026-04-26 11:27:18 +0530

    fix

--- app/Http/Resources/API/CourseResource.php
+++ app/Http/Resources/API/CourseResource.php
@@ -51,7 +51,7 @@ class CourseResource extends JsonResource
       'is_counselling'            => (bool) $this->is_counselling,
       'is_career_guidance'        => (bool) $this->is_career_guidance,
       'allow_installment'         => (bool) $this->allow_installment,
-      'has_payment'               => false,
+      'has_payment'               => true,
 
       // 🏫 Institute (Relationship)
       'institute' => $this->whenLoaded('institute', function () {

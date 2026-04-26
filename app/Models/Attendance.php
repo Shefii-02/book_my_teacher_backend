955189e3865fa0806cc4376191e641f1c8f61555
Shefii-02 <shefii.km@gmail.com>
2026-04-26 13:18:34 +0530

    fix attendance

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,6 +8,8 @@ class Attendance extends Model
 {
   //
 
+  protected $fillable = ['course_id',	'class_id',	'student_id',	'marked_by',	'attendance_date',	'status'];
+
   public function classes()
   {
     return $this->hasOne(CourseClass::class, 'id', 'class_id');

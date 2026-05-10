f709d71b62264be1604c51e75b261b643f884a9a
Shefii-02 <shefii.km@gmail.com>
2026-05-11 00:15:08 +0530

    fix

--- resources/views/home/layouts.blade.php
+++ resources/views/home/layouts.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="/web/assets/css/main.css" />
+    <link rel="stylesheet" href="/web/assets/css/main.css?v=1.0" />
     @stack('styles')
 </head>
 
